2d916e4756bc545cebc5df49b3b00f67456593de
Garezza708 <dwirizky78@gmail.com>
2022-10-30 17:58:48 +0700

    echo

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    echo "aaaaaaaaaaaaa";
+    
+    $name = "Aaaa";
+    $umur = "1000";
+
+    echo "nama saya <strong>$name<strong/>, umur saya $umur <br>";
+
+    $umurkucing = "99999";
+    $selisih = $umurkucing - $umur;
+
+    echo "selisih usia kucing saya dengan saya = $selisih tahun"
+?>
