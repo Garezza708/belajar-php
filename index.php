38a5e261567a8b648488554516f2577d48719878
Garezza708 <dwirizky78@gmail.com>
2022-10-30 18:10:48 +0700

    update

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     $name = "Aaaa";
     $umur = "1000";
 
-    echo "nama saya <strong>$name<strong/>, umur saya $umur <br>";
+    echo "nama saya <strong>$name </strong>, umur saya <strong> $umur </strong> <br>";
 
     $umurkucing = "99999";
     $selisih = $umurkucing - $umur;
 
-    echo "selisih usia kucing saya dengan saya = $selisih tahun"
+    echo "selisih usia kucing saya dengan saya = <strong> $selisih </strong> tahun"
 ?>
